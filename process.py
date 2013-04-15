937899b582297da08acdd606c268165b03e3fac2
Corey Ford <coyotebush22@gmail.com>
2013-04-15 14:28:44 -0700

    script to convert CSVs -> GML

---
+++ process.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import csv
+import igraph
+
+g = igraph.Graph()
+
+with open('repo-attributes.csv', 'rb') as repofile:
+    reader = csv.DictReader(repofile)
+    for repo in reader:
+        g.add_vertex(name=repo['repository_url'],
+            label=repo['repository_url'],
+            language=repo['repository_language'],
+            watchers=int(repo['repository_watchers']))
+
+with open('repo-weights.csv', 'rb') as edgefile:
+    reader = csv.DictReader(edgefile)
+    for edge in reader:
+        g.add_edge(edge['repository1'], edge['repository2'],
+            weight=int(edge['weight']))
+
+print g.summary()
+g.write('repositories.gml')
