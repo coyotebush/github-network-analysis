70684b32a310371be052587c8dadd0c5a629d9ac
Corey Ford <coyotebush22@gmail.com>
2013-04-15 19:49:59 -0700

    use full dataset, optimize

--- repo-attributes.sql
+++ repo-attributes.sql
@@ -5,7 +5,7 @@ FROM
 
 -- Get language and number of stars as of the last event per repository
 (SELECT repository_url, MAX(created_at) AS created_at
-  FROM [publicdata:samples.github_timeline]
+  FROM [githubarchive:github.timeline]
   WHERE PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC('2012-01-01 00:00:00')
     AND PARSE_UTC_USEC(created_at) < PARSE_UTC_USEC('2013-01-01 00:00:00')
   GROUP EACH BY repository_url) AS repo_last_events
@@ -13,8 +13,8 @@ FROM
 JOIN EACH
 
 (SELECT repository_url, repository_language, repository_watchers, created_at
-  FROM [publicdata:samples.github_timeline]) AS repo_languages_watchers
+  FROM [githubarchive:github.timeline]
+  WHERE repository_watchers >= 1000) AS repo_languages_watchers
 ON repo_last_events.repository_url = repo_languages_watchers.repository_url
-  AND repo_last_events.created_at = repo_languages_watchers.created_at
+  AND repo_last_events.created_at = repo_languages_watchers.created_at;
 
-WHERE repository_watchers >= 1000;
--- repo-weights.sql
+++ repo-weights.sql
@@ -1,6 +1,8 @@
 SELECT t1.repository AS repository1, t2.repository AS repository2,
     SUM(SQRT(t1.weight * t2.weight)) AS weight
 FROM
+
+  -- Repository-user weights (identical subquery 1)
   (SELECT user, repo_info.repository_url AS repository, SUM(weight) as weight
   FROM
 
@@ -9,7 +11,7 @@ FROM
       FROM
 
       (SELECT repository_url, MAX(created_at) AS created_at
-        FROM [publicdata:samples.github_timeline]
+        FROM [githubarchive:github.timeline]
         WHERE PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC('2012-01-01 00:00:00')
           AND PARSE_UTC_USEC(created_at) < PARSE_UTC_USEC('2013-01-01 00:00:00')
         GROUP EACH BY repository_url) AS repo_last_events
@@ -17,11 +19,10 @@ FROM
       JOIN EACH
 
       (SELECT repository_url, repository_watchers, created_at
-        FROM [publicdata:samples.github_timeline]) AS repo_watchers
+        FROM [githubarchive:github.timeline]
+        WHERE repository_watchers >= 1000) AS repo_watchers
       ON repo_last_events.repository_url = repo_watchers.repository_url
         AND repo_last_events.created_at = repo_watchers.created_at
-
-      WHERE repository_watchers >= 1000
     ) AS repo_info
 
     JOIN EACH
@@ -31,7 +32,7 @@ FROM
 
       -- Pushes
       (SELECT actor AS user, repository_url, COUNT(repository_url) AS weight
-        FROM [publicdata:samples.github_timeline]
+        FROM [githubarchive:github.timeline]
         WHERE type='PushEvent' 
           AND PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC('2012-01-01 00:00:00')
           AND PARSE_UTC_USEC(created_at) < PARSE_UTC_USEC('2013-01-01 00:00:00')
@@ -40,7 +41,7 @@ FROM
       -- Merged pull requests
       (SELECT payload_pull_request_user_login AS user, repository_url,
             COUNT(repository_url) AS weight
-        FROM [publicdata:samples.github_timeline]
+        FROM [githubarchive:github.timeline]
         WHERE type='PullRequestEvent'
           AND payload_action='closed'
           AND payload_pull_request_merged='true'
@@ -54,6 +55,7 @@ FROM
   
   JOIN EACH
 
+  -- Repository-user weights (identical subquery 2)
   (SELECT user, repo_info.repository_url AS repository, SUM(weight) as weight
   FROM
 
@@ -62,7 +64,7 @@ FROM
       FROM
 
       (SELECT repository_url, MAX(created_at) AS created_at
-        FROM [publicdata:samples.github_timeline]
+        FROM [githubarchive:github.timeline]
         WHERE PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC('2012-01-01 00:00:00')
           AND PARSE_UTC_USEC(created_at) < PARSE_UTC_USEC('2013-01-01 00:00:00')
         GROUP EACH BY repository_url) AS repo_last_events
@@ -70,11 +72,10 @@ FROM
       JOIN EACH
 
       (SELECT repository_url, repository_watchers, created_at
-        FROM [publicdata:samples.github_timeline]) AS repo_watchers
+        FROM [githubarchive:github.timeline]
+        WHERE repository_watchers >= 1000) AS repo_watchers
       ON repo_last_events.repository_url = repo_watchers.repository_url
         AND repo_last_events.created_at = repo_watchers.created_at
-
-      WHERE repository_watchers >= 1000
     ) AS repo_info
 
     JOIN EACH
@@ -84,7 +85,7 @@ FROM
 
       -- Pushes
       (SELECT actor AS user, repository_url, COUNT(repository_url) AS weight
-        FROM [publicdata:samples.github_timeline]
+        FROM [githubarchive:github.timeline]
         WHERE type='PushEvent' 
           AND PARSE_UTC_USEC(created_at) >= PARSE_UTC_USEC('2012-01-01 00:00:00')
           AND PARSE_UTC_USEC(created_at) < PARSE_UTC_USEC('2013-01-01 00:00:00')
@@ -93,7 +94,7 @@ FROM
       -- Merged pull requests
       (SELECT payload_pull_request_user_login AS user, repository_url,
             COUNT(repository_url) AS weight
-        FROM [publicdata:samples.github_timeline]
+        FROM [githubarchive:github.timeline]
         WHERE type='PullRequestEvent'
           AND payload_action='closed'
           AND payload_pull_request_merged='true'
@@ -108,7 +109,5 @@ FROM
 
 WHERE t1.repository < t2.repository
 GROUP EACH BY repository1, repository2;
-
--- publicdata:samples.github_timeline - 460 repo pairs
 
 -- vim: sw=2 ts=2 sts=2 et
