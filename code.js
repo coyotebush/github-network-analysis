bec2c5b350679b162e1cdb836845d46e92623dff
Corey Ford <coyotebush22@gmail.com>
2013-04-17 16:21:08 -0700

    indentation/organization tweaks

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 $(function () {
   graph = sigma.init(document.getElementById('graph')).drawingProperties({
     defaultLabelColor: '#fff',
-      defaultLabelSize: 12,
-      defaultLabelBGColor: '#fff',
-      defaultLabelHoverColor: '#000',
-      labelThreshold: 6,
-      defaultEdgeType: 'curve'
+    defaultLabelSize: 12,
+    defaultLabelBGColor: '#fff',
+    defaultLabelHoverColor: '#000',
+    labelThreshold: 6,
+    defaultEdgeType: 'curve'
   }).graphProperties({
     minNodeSize: 1,
     maxNodeSize: 10,
@@ -14,12 +14,12 @@ $(function () {
   }).mouseProperties({
     maxRatio: 32
   });
+  graph.bind('upnodes', function(e) {
+    window.open(graph.getNodes(e.content[0]).attr.attributes.name);
+  });
   graph.parseJson('repositories.json', function() {
     graph.iterEdges(function(e){
       e.size=e.weight;
-    });
-    graph.bind('upnodes', function(e) {
-      window.open(graph.getNodes(e.content[0]).attr.attributes.name);
     });
     graph.draw();
   });
