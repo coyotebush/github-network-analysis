20dd2ee22f56f6fd0c37876954fc2dc1f9e1b3fa
Corey Ford <coyotebush22@gmail.com>
2013-05-03 18:33:43 -0700

    reformat js

--- code.js
+++ code.js
@@ -1,26 +1,24 @@
-$(function () {
-  graph = sigma.init(document.getElementById('graph')).drawingProperties({
-    defaultLabelColor: '#fff',
-    defaultLabelSize: 12,
-    defaultLabelBGColor: '#fff',
-    defaultLabelHoverColor: '#000',
-    labelThreshold: 4,
-    defaultEdgeType: 'curve'
-  }).graphProperties({
-    minNodeSize: 1,
-    maxNodeSize: 10,
-    minEdgeSize: 0.3,
-    maxEdgeSize: 30
-  }).mouseProperties({
-    maxRatio: 32
+var graph = sigma.init(document.getElementById('graph')).drawingProperties({
+  defaultLabelColor: '#fff',
+  defaultLabelSize: 12,
+  defaultLabelBGColor: '#fff',
+  defaultLabelHoverColor: '#000',
+  labelThreshold: 4,
+  defaultEdgeType: 'curve'
+}).graphProperties({
+  minNodeSize: 1,
+  maxNodeSize: 10,
+  minEdgeSize: 0.3,
+  maxEdgeSize: 30
+}).mouseProperties({
+  maxRatio: 32
+}).bind('upnodes', function(e) {
+  window.open(graph.getNodes(e.content[0]).attr.attributes.name);
+});
+
+graph.parseJson('repositories.json', function() {
+  graph.iterEdges(function(e){
+    e.size=e.weight;
   });
-  graph.bind('upnodes', function(e) {
-    window.open(graph.getNodes(e.content[0]).attr.attributes.name);
-  });
-  graph.parseJson('repositories.json', function() {
-    graph.iterEdges(function(e){
-      e.size=e.weight;
-    });
-    graph.draw();
-  });
+  graph.draw();
 });
